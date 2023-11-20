c8dcb60a9723dec8e9bf06188c97c2dbf541e12b
nmul1801 <nicholas.m.mulligan@gmail.com>
2023-11-20 15:54:23 -0500

    allowed hosts

--- fantasy/settings.py
+++ fantasy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yel2d0wshslvw7kox^nqum391vi!plf!8ur&%(l#1=st+fk$#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nmul1801.pythonanywhere.com']
 
 
 # Application definition
